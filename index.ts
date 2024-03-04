41a9c10adc87d0d91a21c443718a7a42e5f72da8
devjiwonchoi <devjiwonchoi@gmail.com>
2024-03-04 10:43:34 +0900

    specify baseurl

--- index.ts
+++ index.ts
@@ -1,19 +1,15 @@
 import { transform } from '@swc/core'
 
 const input = `
-import { foo } from '@/foo'
 import { bar } from 'src/bar'
 
-console.log(foo, bar)
+console.log(bar)
 `
 
 async function main() {
   const { code } = await transform(input, {
     jsc: {
       baseUrl: process.cwd(),
-      paths: {
-        '@/*': ['./src/*'],
-      },
     },
   })
 
